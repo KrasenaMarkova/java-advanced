f1e60f76505c908503be118657c170aa33a13a98
Krasena Markova <kraschoo@abv.bg>
2026-09-10 17:16:49 +0300

    feat: add predicate for names solution

---
+++ src/05.FunctionalProgrammingExercises/PredicateForNames06.java
@@ -0,0 +1,31 @@
+package FunctionalProgrammingExercises05;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class PredicateForNames06 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int requiredLength = Integer.parseInt(scanner.nextLine());
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+")).toList();
+
+        // Predicate: аргумент -> израз/действие, който връща булева стойност
+        Predicate<String> predicate = name -> name.length() <= requiredLength;
+
+        //Начин 1:
+//        names.stream()
+//                .filter(predicate)
+//                .forEach(name -> System.out.println(name));
+
+        // Начин 2:
+        for (String name : names) {
+            if (predicate.test(name)) {
+                System.out.println(name);
+            }
+        }
+    }
+}
