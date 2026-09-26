afe274733dfc8e99b5a3e5f23e2a5742e38dc01b
Krasena Markova <kraschoo@abv.bg>
2026-09-26 12:17:28 +0300

    Add Raw Data solution

---
+++ src/06.DefiningClassesExercises/MainRawData04.java
@@ -0,0 +1,27 @@
+package DefiningClassesExercises06;
+
+public class MainRawData04 {
+    private double pressure;
+    private int age;
+
+    public void Tire(double pressure, int age) {
+        this.pressure = pressure;
+        this.age = age;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
